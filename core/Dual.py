5e8f30689e070191b375ff7c64199837429442f5
jguu0056 <jguu0056@student.monash.edu>
2026-03-19 03:03:03 +0800

    update

---
+++ core/Dual.py
@@ -0,0 +1,48 @@
+import math
+
+class Dual:
+    def __init__(self,val,der = 0.0):
+        self.val = val
+        self.der = der
+
+    def __add__(self, other):
+        # (u + v)' = u' + v'
+
+        other = other if isinstance(other, Dual) else Dual(other)
+        return Dual(self.val+other.val,self.der+other.der)
+
+    def __sub__(self, other):
+        # (u - v)' = u' - v'
+
+        other = other if isinstance(other, Dual) else Dual(other)
+        return Dual(self.val - other.val, self.der - other.der)
+
+    def __rsub__(self, other):
+        return Dual(other - self.val, -self.der)
+
+
+    def __mul__(self, other):
+        # (uv)' = u'v + uv'
+
+        other = other if isinstance(other, Dual) else Dual(other)
+        return Dual(self.val * other.val,
+                    self.der * other.val + self.val * other.der)
+
+    def __truediv__(self, other):
+        # (u/v)' = (u'v - uv') / v^2
+
+        other = other if isinstance(other, Dual) else Dual(other)
+        return Dual(self.val / other.val,
+                    (self.der * other.val - self.val * other.der) / (other.val**2))
+
+    def __rtruediv__(self, other):
+        return  Dual(other.val/self.val,
+                     - (other * self.der) / (self.val ** 2))
+
+    def __pow__(self, n):
+
+        # [u(x)^n]' = n * u(x)^(n-1) * u'(x)
+        return Dual(self.val**n, n * (self.val**(n-1)) * self.der)
+
+    def __repr__(self):
+        return f"Dual(val={self.val:.4f}, der={self.der:.4f})"
---
+++ core/Functions.py
@@ -0,0 +1,39 @@
+import math
+from Dual import Dual
+
+def sin(x):
+    """ f(x) = sin(x), f'(x) = cos(x) * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    return Dual(math.sin(x.val), math.cos(x.val) * x.der)
+
+def cos(x):
+    """ f(x) = cos(x), f'(x) = -sin(x) * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    return Dual(math.cos(x.val), -math.sin(x.val) * x.der)
+
+def exp(x):
+    """ f(x) = e^x, f'(x) = e^x * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    val = math.exp(x.val)
+    return Dual(val, val * x.der)
+
+def log(x):
+    """ f(x) = ln(x), f'(x) = (1/x) * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    return Dual(math.log(x.val), (1.0 / x.val) * x.der)
+
+def sinh(x):
+    """ f(x) = sinh(x), f'(x) = cosh(x) * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    return Dual(math.sinh(x.val), math.cosh(x.val) * x.der)
+
+def cosh(x):
+    """ f(x) = cosh(x), f'(x) = sinh(x) * x' """
+
+    x = x if isinstance(x, Dual) else Dual(x)
+    return Dual(math.cosh(x.val), math.sinh(x.val) * x.der)
